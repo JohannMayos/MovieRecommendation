ff3b7d0e6e674e723749e40974f101ba067de8e3
Johann Mayos Lopes da Silva <42842871+JohannMayos@users.noreply.github.com>
2024-01-23 17:41:59 -0400

    Choosen Genres List to Recommendation Func Integration Added

--- front.py
+++ front.py
@@ -3,13 +3,7 @@ import requests
 import pandas as pd
 import random
 import numpy as np
-
-# Configuração da sessão para armazenar variáveis
-if 'avaliacoes_anteriores' not in st.session_state:
-    st.session_state.avaliacoes_anteriores = []
-    st.session_state.numero_avaliacoes = 0
-    st.session_state.inputs_nome_jogo = {}
-    st.session_state.rec_list = []
+from back import recommend_by_genres
 
 # Título da aplicação
 st.title("Avaliação de Filmes")
@@ -20,26 +14,35 @@ st.write("Bem-vindo à nossa aplicação, nela você indicará quais gêneros de
 
 st.header("Gêneros de Filmes")
 
-checkbox_action = st.checkbox("Ação")
-checkbox_adventure = st.checkbox("Aventura")
-checkbox_fantasy = st.checkbox("Fantasia")
-checkbox_Scifi = st.checkbox("Ficção Científica")
-checkbox_crime = st.checkbox("Crime")
-checkbox_drama = st.checkbox("Drama")
-checkbox_thriller = st.checkbox("Thriller")
-checkbox_anim = st.checkbox("Animação")
-checkbox_family = st.checkbox("Família")
-checkbox_romance = st.checkbox("Romance")
-checkbox_horror = st.checkbox("Horror")
-checkbox_mistery = st.checkbox("Mistério")
-checkbox_history = st.checkbox("História")
-checkbox_war = st.checkbox("Guerra")
-checkbox_comedy = st.checkbox("Comédia")
-checkbox_western = st.checkbox("Velho Oeste")
-checkbox_music = st.checkbox("Música")
+genres_dict = {
+    "Ação": False,
+    "Aventura": False,
+    "Fantasia": False,
+    "Ficção Científica": False,
+    "Crime": False,
+    "Drama": False,
+    "Thriller": False,
+    "Animação": False,
+    "Família": False,
+    "Romance": False,
+    "Horror": False,
+    "Mistério": False,
+    "História": False,
+    "Guerra": False,
+    "Comédia": False,
+    "Velho Oeste": False,
+    "Música": False
+}
+
+for genre in genres_dict:
+    genres_dict[genre] = st.checkbox(genre)
+
+
+selected_genres = [genre for genre, state in genres_dict.items() if state]
+
 
 
 if st.button("Obter Recomendações"):
 
-  st.write("Recomendações")
+  st.write(recommend_by_genres(selected_genres))
 
