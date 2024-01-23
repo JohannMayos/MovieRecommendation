68b6ba9514a44d8452d8804b7cdf87c485d80804
Larissa Roque Carvalho <50839355+LarissaRC@users.noreply.github.com>
2024-01-23 16:33:05 -0400

    Recommend by genres

--- back.py
+++ back.py
@@ -7,45 +7,53 @@ import json
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Carregar o dataset e separar apenas as colunas de interesse
-movies = pd.read_csv('dataset.csv')
-movies = movies[['genres', 'original_title', 'vote_average']]
+'''
+Gêneros disponíveis:
+    ['Action', 'Adventure', 'Fantasy', 'Science Fiction', 'Crime', 'Drama', 'Thriller', 'Animation',
+    'Family', 'Romance', 'Horror', 'Mystery', 'History', 'War', 'Comedy', 'Western', 'Music']
+'''
 
-# Editar a coluna de gêneros
-genres = []
+def recommend_by_genres(user_profile):
 
-for value in movies.genres:
-    movie_genres = json.loads(value)
-    movie_genres_filtered = []
-    for genre in movie_genres:
-        movie_genres_filtered.append(genre["name"])
-    genres.append('|'.join(movie_genres_filtered))
+    # Carregar o dataset e separar apenas as colunas de interesse
+    movies = pd.read_csv('dataset.csv')
+    movies = movies[['genres', 'original_title']]
 
-movies['genres'] = genres
-        
-# Verificar a popularidade de cada gênero retornado
-genre_popularity = (movies.genres.str.split('|').explode().value_counts().sort_values(ascending=False))
+    # Editar a coluna de gêneros
+    genres = []
 
-# Obter valores TF-IDF
-tfidf = TfidfVectorizer(stop_words='english')
-tfidf_matrix = tfidf.fit_transform(movies['genres'])
-print(tfidf_matrix.shape)
+    for value in movies.genres:
+        movie_genres = json.loads(value)
+        movie_genres_filtered = []
+        for genre in movie_genres:
+            movie_genres_filtered.append(genre["name"])
+        genres.append('|'.join(movie_genres_filtered))
 
-print("Gênetros: ", tfidf.get_feature_names_out())
+    movies['genres'] = genres
+            
+    # Verificar a popularidade de cada gênero retornado
+    genre_popularity = (movies.genres.str.split('|').explode().value_counts().sort_values(ascending=False))
 
-print("Matriz esparsa de ID-IDF:")
-print(pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf.get_feature_names_out(), index=movies.original_title).sample(10, axis=0))
+    # Criação de uma linha para a tabela de filmes com os gêneros informados pelo usuário
+    simulated_movie = {'genres': '|'.join(user_profile), 'original_title': 'FAKE MOVIE'}
 
-# Calcular similaridade entre vetores - Similaridade do Cosseno
-cosine_sim = cosine_similarity(tfidf_matrix)
+    # Adição do "filme" criado na tabela de filmes, para que possa ser processado com todos
+    movies = movies._append(simulated_movie, ignore_index=True)
 
-cosine_sim_df = pd.DataFrame(cosine_sim, index=movies['original_title'], columns=movies['original_title'])
-print("Shape: ", cosine_sim_df.shape)
-print(cosine_sim_df.sample(5, axis=1).round(2))
+    # Obter valores TF-IDF
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(movies['genres'])
 
-# Função para retornar os filmes mais similares a uma lista de gêneros
-# informada pelo usuário
-def recommend(i, M, items, k=10):
+    # Calcular similaridade entre vetores - Similaridade do Cosseno
+    cosine_sim = cosine_similarity(tfidf_matrix)
+    cosine_sim_df = pd.DataFrame(cosine_sim, index=movies['original_title'], columns=movies['original_title'])
+
+    # Realizar a busca de filmes similares ao filme criado com os gostos do usuário
+    recommendations = recommend_by_movie('FAKE MOVIE', cosine_sim_df, movies[['original_title', 'genres']])
+
+    return recommendations
+
+def recommend_by_movie(i, M, items, k=10):
     # Obter uma lista de índices de filmes mais similares ao perfil alvo
     ix = M.loc[:,i].to_numpy().argpartition(range(-1, -k, -1))
     # Obter uma lista com os filmes mais similares
@@ -55,7 +63,11 @@ def recommend(i, M, items, k=10):
 
     return pd.DataFrame(closest).merge(items).head(k)
 
-recommendations = recommend('Jurassic Park', cosine_sim_df, movies[['original_title', 'genres']])
+################ Teste de uso do sistema #################
 
-print("\nRecomendações:")
+# Gêneros indicados pelo usuário
+user_profile = ['Adventure', 'Science Fiction']
+recommendations = recommend_by_genres(user_profile)
+
+print("Filmes recomendados para este usuário:")
 print(recommendations)
